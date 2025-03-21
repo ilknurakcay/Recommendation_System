34c979d4b58daef7579db4f2121a5a5c1c06fc2c
ilknurakcay <108659493+ilknurakcay@users.noreply.github.com>
2025-03-21 10:01:23 +0300

    comment update

--- node2vec_embeddings.py
+++ node2vec_embeddings.py
@@ -8,10 +8,9 @@ import pandas as pd
 import pickle
     
 
-#If you want to train model, you can use folowing 4  lines.
+#If you want to run train model, you can use folowing 4  lines.
 #node2vec = Node2Vec(graph=G_subgraph, dimensions=64, walk_length=5, num_walks=10, workers=8)
 #model = node2vec.fit(window=10, min_count=1, batch_words=4)
-# Save to model
 #model.save("/content/drive/MyDrive/rec_sys_drive/node2vec_model.bin")
 
 #I trained model before and I will use this.
@@ -31,7 +30,7 @@ def get_job_embeddings(G_subgraph, model):
 
   Parameters:
   -G_subgraph: Subgraph containing job and user nodes.
-  -model:A trained embedding model that provides vector 
+  -model: A trained embedding model that provides vector 
       representations for job nodes.
   """
   user_count = sum(1 for node, data in G_subgraph.nodes(data=True) if data.get("type") == "user")
@@ -56,8 +55,8 @@ def get_position_name(job_id, item_info):
     Returns the position name corresponding to the given job ID.
 
     Parameters:
-    - job_id (str): The job's ID, e.g., 'J_4031363'.
-    - item_info (DataFrame): A pandas DataFrame containing job information.
+    - job_id: The job's ID, e.g., 'J_4031363'.
+    - item_info: A pandas DataFrame containing job information.
 
     """
     item_id = job_id.replace("J_", "")  
@@ -70,28 +69,28 @@ def calculate_average_similarity(job_embeddings, G_subgraph, item_info, num_pair
     Cosine similarity is used for the similarity calculation.
 
     Parameters:
-    - job_embeddings : A dictionary containing the embedding vectors for each job. 
+    - job_embeddings : Containing the embedding vectors for each job. 
       The keys are job IDs, and the values are the embedding vectors.
-    - G_subgraph : A subgraph object containing the job and user nodes.
-    - item_info :A pandas DataFrame containing job information, including position names.
+    - G_subgraph : A subgraph include job and user nodes.
+    - item_info : item_info excel.
     - num_pairs : The number of random job pairs to compute. Default is 5.
     """
     similarities = []
     
-    #Get "job" nodes in subgraph
+    # Get "job" nodes in subgraph
     job_nodes = [node for node, data in G_subgraph.nodes(data=True) if data.get("type") == "job"]
     if len(job_nodes) < 2:
         print("There are insufficient job nodes!")
         return None
 
-    # Generate all job pairs and randomly select 10
+    # Generate all job pairs and randomly select 5
     all_pairs = list(itertools.combinations(job_nodes, 2))  
     selected_pairs = random.sample(all_pairs, min(len(all_pairs), num_pairs))
 
     # Get embedding vectors of selected pairs
     job_vectors = {job: job_embeddings[job] for job in job_nodes if job in job_embeddings}
 
-    # Caclulate cosine similarity 
+    # Calculate cosine similarity 
     for job1, job2 in selected_pairs:
         if job1 not in job_vectors or job2 not in job_vectors:
             print(f"WARNING: {job1} or {job2} is not in job_embeddings!")
